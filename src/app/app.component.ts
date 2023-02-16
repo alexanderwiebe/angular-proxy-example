0683ef73d407bc07e8463de247305bd32c3230c8
Alexander Wiebe <alexanderjwiebe@gmail.com>
2023-02-16 17:12:51 -0600

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { TitleService } from './title.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-app';
+  titleNG = 'angular-app';
+
+  title1 = {};
+  title2 = {};
+
+  constructor(public title: TitleService) {}
+
+  getTitle1() {
+    this.title.getTitle1().subscribe((newTitle) => (this.title1 = newTitle));
+  }
+
+  getTitle2() {
+    this.title.getTitle1().subscribe((newTitle) => (this.title2 = newTitle));
+  }
 }
